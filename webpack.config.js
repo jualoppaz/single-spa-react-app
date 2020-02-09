48521a2902037e2856fbf29567f38a5caf75a015
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-09 15:00:57 +0100

    Add webpack config file for build app as library

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: ['src/singleSpaEntry.js'],
+  output: {
+    library: 'single-spa-react-app',
+    libraryTarget: 'umd',
+    filename: 'single-spa-react-app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css|\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  node: {
+    fs: 'empty',
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist'],
+    }),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
+  devtool: 'source-map',
+  externals: [],
+  devServer: {
+    historyApiFallback: true,
+    writeToDisk: true,
+  },
+};
